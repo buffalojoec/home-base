2df47b4aae93fe49192cd057609e7105d529ec5b
jpcaulfi <jcaulfield135@gmail.com>
2022-04-21 17:31:06 -0400

    initial commit

---
+++ src/main/java/com/jpcaulfi/homebase/HomeBaseApplication.java
@@ -0,0 +1,13 @@
+package com.jpcaulfi.homebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBaseApplication.class, args);
+	}
+
+}
